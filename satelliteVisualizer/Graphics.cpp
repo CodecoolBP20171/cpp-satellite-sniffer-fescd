39a0161098cad15d654e75f7a3a4c12ffdd63164
Regina Csaszar <regina.csaszar@gmail.com>
2017-12-05 12:21:01 +0100

    graphics class added

---
+++ satelliteVisualizer/Graphics.cpp
@@ -0,0 +1,85 @@
+#include "Graphics.h"
+#include <iostream>
+
+
+
+Graphics::Graphics()
+{
+	if (!init()) { printf("Failed to initialize!\n"); }
+	if (!loadMedia()) { printf("Failed to load media!\n"); }
+}
+
+Graphics::~Graphics() {}
+
+bool Graphics::init() {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+		printf("Warning: Linear texture filtering not enabled!");
+	}
+
+	std::unique_ptr<SDL_Window, sdl_deleter> window(
+		SDL_CreateWindow("Satellite visualizer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			1029, 873, SDL_WINDOW_SHOWN),
+		sdl_deleter());
+	if (!window) {
+		printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	gWindow = std::move(window);
+
+	std::unique_ptr<SDL_Renderer, sdl_deleter> renderer(
+		SDL_CreateRenderer(gWindow.get(), -1, SDL_RENDERER_ACCELERATED),
+		sdl_deleter());
+
+	if (!renderer) {
+		printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	gRenderer = std::move(renderer);
+
+	SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
+
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) & imgFlags)) {
+		printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+		return false;
+	}
+	return true;
+}
+
+bool Graphics::loadMedia() {
+	
+	SDL_Surface* loadedSurface = nullptr;
+
+	std::string path = "";
+
+	loadedSurface = IMG_Load(path.c_str());
+
+	std::unique_ptr<SDL_Texture, sdl_deleter> newTexture(
+			SDL_CreateTextureFromSurface(gRenderer.get(), loadedSurface),
+			sdl_deleter());
+
+	background = std::move(newTexture);
+	SDL_FreeSurface(loadedSurface);
+
+	SDL_Rect bg = { 0, 0, 1029, 873 };
+	SDL_RenderCopy(gRenderer.get(), background.get(), nullptr, &bg);
+	SDL_RenderPresent(gRenderer.get());
+	SDL_Delay(6000);
+	return true;
+}
+
+void Graphics::close() {
+	//this order is important!
+	background.reset();
+	gRenderer.reset();
+	gWindow.reset();
+
+	IMG_Quit();
+	SDL_Quit();
+}
---
+++ satelliteVisualizer/Graphics.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include <SDL.h>
+#include <SDL_>
+#include <cstdio>
+#include <string>
+#include <map>
+
+class Graphics
+{
+public:
+	Graphics();
+	~Graphics();
+
+	void close();
+
+private:
+
+	struct sdl_deleter
+	{
+		void operator()(SDL_Window *p) const { SDL_DestroyWindow(p); }
+		void operator()(SDL_Renderer *p) const { SDL_DestroyRenderer(p); }
+		void operator()(SDL_Texture *p) const { SDL_DestroyTexture(p); }
+	};
+
+	std::unique_ptr<SDL_Window, sdl_deleter> gWindow = nullptr;
+	std::unique_ptr<SDL_Renderer, sdl_deleter> gRenderer = nullptr;
+
+	std::unique_ptr<SDL_Texture, sdl_deleter> background = nullptr;
+	
+	bool init();
+	bool loadMedia();
+
+};
